3a7b036a04b7a7833ebe0567ad81a59ef4b7db91
Chris-G-5150 <distort_everything@proton.me>
2023-12-13 10:17:13 +0000

    challenge 7 completed with error handling

--- src/components/ArticleVotes.jsx
+++ src/components/ArticleVotes.jsx
@@ -1,8 +1,11 @@
 import axios from "axios";
 import { useState } from "react";
+import ErrorPopup from "./ErrorPopup";
+
 
 export default function ArticleVotes({ article }) {
     const [votes, setVotes] = useState(article.votes);
+    const [errStatus, setErrStatus] = useState(null)
 
     console.log(article.article_id, "article_id in article votes");
 
@@ -17,9 +20,19 @@ export default function ArticleVotes({ article }) {
                 { inc_votes: 1 }
             )
             .catch((err) => {
-                console.log(err);
+                console.log(err)
+                setErrStatus(err.message);
             });
     }
+
+    function popupErrHandler(){
+        setErrStatus(null)
+    }
+
+    if (errStatus !== null){
+        return (<ErrorPopup errStatus={errStatus} onClose={popupErrHandler}/>)
+    }
+    else{
 
     return (
         <div className="article_votes_container">
@@ -27,4 +40,5 @@ export default function ArticleVotes({ article }) {
             <button onClick={handleVoteUp}>+</button>
         </div>
     );
+    }
 }
---
+++ src/components/ErrorPopup.jsx
@@ -0,0 +1,18 @@
+import "./CSS/ErrorPopup.css"
+
+export default function ErrorPopup({errStatus, onClose}){
+    return(
+    <div className="error_popup_background">
+    <section className="error_popup_container">
+    <header><h2>An error occurred</h2></header>
+    <div className="error_popup_msg_container"><h3>msg:</h3>
+    <div className="error_popup_msg">
+    <p>{errStatus}</p>
+    <button type="button" onClick={onClose}>close</button>
+    </div>
+    </div>
+    </section>
+    </div>)
+
+
+}
